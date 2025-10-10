93dd7d260f7dc3495869495f91c600d69c005a6e
Millie Solem <61805186+milliesolem@users.noreply.github.com>
2025-10-10 16:40:32 +0200

    detect-fash: added dhh and hyprland support

--- src/detect-fash/detect-fash.c
+++ src/detect-fash/detect-fash.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
 #include "alloc-util.h"
 #include "build.h"
 #include "log.h"
@@ -16,52 +15,100 @@ static bool arg_quiet = false;
 static enum {
 	ANY_FASCISM,
 	ONLY_LADYBIRD,
-	ONLY_OMARCHY
+	ONLY_OMARCHY,
+	ONLY_HYPRLAND,
+	ONLY_DHH
 } arg_mode = ANY_FASCISM;
 
+/* detects if os-release is omarchy */
 static int detect_omarchy() {
 	const char *term = "omarchy";
 	const int len = 100;
 
-	if (access("/etc/os-release", F_OK) != 0){
+	/* if we cannot access os-release we cannot check */
+	if (access("/etc/os-release", F_OK) != 0)
 		return -1;
-	}
-	osfile = fopen("/etc/os-release", "r");
+
+	FILE *osfile = fopen("/etc/os-release", "r");
 	char os[len];
 	fgets(os, len, osfile);
-	if (strcasestr(os, term) != NULL){
+	if (strcasestr(os, term) != NULL)
 		return 1;
-	}
+
 	return 0;
 }
 
+/* detects if the binary "ladybird" exists in $PATH */
 static int detect_ladybird() {
 	const char *term = "/ladybird";
 	const int spath_maxlen = 256;
 
-	char *env_path = strdup(getenv("PATH"));
-	if (env_path == NULL){
+	char *PATH = getenv("PATH");
+	if (PATH == NULL)
 		return -1;
-	}
+
+	char *env_path = strdup(PATH);
 	char *path_iter = env_path;
 	char *p = NULL;
 	char path_buffer[spath_maxlen];
 
-	// iterate through $PATH to check if a file `ladybird` exists
+	/* iterate through $PATH to check if a file `ladybird` exists */
 	do {
 		p = strchr(env_path, ':');
-		if (p != NULL) {
+		if (p != NULL)
 			p[0] = 0;
-		}
-		// we use strncpy to keep the code safe :))
+
+		/* we use strncpy to keep the code safe :))*/
 		strncpy(path_buffer, s, spath_maxlen);
 		strcat(path_buffer, term);
-		if (access(path_buffer, F_OK) == 0) {
+		if (access(path_buffer, F_OK) == 0)
 			return 1;
-		}
+
 		printf("Path in $PATH: %s\n", s);
 		s = p + 1;
 	} while (p != NULL);
+	return 0;
+}
+
+/* detects if hyprland is installed */
+static int detect_hyprland() {
+	char *hyprland_config = "/hypr/hyprland.conf";
+	char *XDG_CONFIG_HOME = getenv("XDG_CONFIG_HOME");
+	if (XDG_CONFIG_HOME == NULL)
+		return -1; 
+	char *hyprland_abs_path = strdup(XDG_CONFIG_HOME);
+	strcat(hyprland_abs_path, hyprland_config);
+	if (access(hyprland_abs_path, F_OK) != 0)
+		return 1;
+	return 0;
+}
+
+/* detects if this is dhh's computer using his ssh pubkey */
+static int detect_dhh() {
+	/* fingerprint of dhh's ssh public key */
+	char *dhh_fingerprint = "SHA256:YCKX7xo5Hkihy/NVH5ang8Oty9q8Vvqu4sxI7EbDxPg";
+
+	/* get the home directory */
+	char *HOME = getenv("HOME");
+	if (HOME == NULL)
+		return -1;
+
+	/* check if we have read access to the public key on disk */
+	char *ssh_pubkey = '/.ssh/id_ed25519.pub';
+	char *ssh_pubkey_abs_path = strdup(HOME);
+	strcat(ssh_pubkey_abs_path, ssh_pubkey);
+	if (access(ssh_pubkey_abs_path, F_OK) == 0)
+		return -1;
+	
+	/* generate a fingerprint of it */
+	char *get_fingerprint_cmd = "ssh-keygen -E 256 -lf ";
+	strcat(get_fingerprint_cmd, ssh_pubkey_abs_path);
+	char fingerprint[70];
+	fgets(fingerprint, 70, popen(get_fingerprint_cmd, "r"));
+
+	/* comare it to DHH's fingerprint */
+	if (strstr(fingerprint, dhh_fingerprint) != NULL)
+		return 1;
 	return 0;
 }
 
@@ -80,6 +127,8 @@ static int help(void) {
 		   "  -q --quiet        	Quiet mode\n"
 	       "  -o --omarchy        	Only detect omarchy\n"
 	       "  -l --ladybird         Only detect ladybird\n"
+		   "  -y --hyprland         Only detect hyprland\n"
+		   "  -d --dhh              Only detect dhh\n"
 	       "\nSee the %s for details.\n",
 	       program_invocation_short_name,
 	       link);
@@ -92,7 +141,9 @@ static int parse_argv(int argc, char *argv[]) {
 	enum {
 		ARG_VERSION = 0x100,
 		ARG_OMARCHY,
-		ARG_LADYBIRD
+		ARG_LADYBIRD,
+		ARG_HYPRLAND,
+		ARG_DHH
 	};
 
 	static const struct option options[] = {
@@ -100,6 +151,8 @@ static int parse_argv(int argc, char *argv[]) {
 		{ "version",       no_argument, NULL, ARG_VERSION       },
 		{ "omarchy",       no_argument, NULL, 'o'               },
 		{ "ladybird",      no_argument, NULL, 'l'               },
+		{ "hyprland",      no_argument, NULL, 'y'               },
+		{ "dhh",           no_argument, NULL, 'd'               },
 		{}
 	};
 
@@ -108,7 +161,7 @@ static int parse_argv(int argc, char *argv[]) {
 	assert(argc >= 0);
 	assert(argv);
 
-	while ((c = getopt_long(argc, argv, "hqol", options, NULL)) >= 0)
+	while ((c = getopt_long(argc, argv, "hqolyd", options, NULL)) >= 0)
 
 		switch (c) {
 
@@ -129,6 +182,14 @@ static int parse_argv(int argc, char *argv[]) {
 		case 'o':
 			arg_mode = ONLY_OMARCHY;
 			break;
+		
+		case 'y':
+			arg_mode = ONLY_HYPRLAND;
+			break;
+		
+		case 'd':
+			arg_mode = ONLY_DHH;
+			break;
 
 		case '?':
 			return -EINVAL;
@@ -136,16 +197,12 @@ static int parse_argv(int argc, char *argv[]) {
 		default:
 			assert_not_reached();
 		}
-
-	if (optind < argc)
-		return log_error_errno(SYNTHETIC_ERRNO(EINVAL),
-				       "%s takes no arguments.",
-				       program_invocation_short_name);
-
 	return 1;
 }
 
 static int run(int argc, char *argv[]) {
+	int dhh = 0;
+	int hyprland = 0;
 	int ladybird = 0;
 	int omarchy = 0;
 	int fascism = 0;
@@ -166,28 +223,46 @@ static int run(int argc, char *argv[]) {
 		omarchy = detect_omarchy();
 		fascism = omarchy;
 		if (omarchy < 0)
-			return log_error_errno(v, "Failed to check for omarchy: %m");
+			return log_error_errno(fascism, "Failed to check for omarchy: %m");
 		break;
 
 	case ONLY_LADYBIRD:
 		ladybird = detect_ladybird();
 		fascism = ladybird;
 		if (ladybird < 0)
-			return log_error_errno(v, "Failed to check for ladybird: %m");
+			return log_error_errno(fascism, "Failed to check for ladybird: %m");
+		break;
+	
+	case ONLY_HYPRLAND:
+		ladybird = detect_hyprland();
+		fascism = ladybird;
+		if (ladybird < 0)
+			return log_error_errno(fascism, "Failed to check for hyprland: %m");
+		break;
+	
+	case ONLY_DHH:
+		dhh = detect_dhh();
+		fascism = dhh;
+		if (dhh < 0)
+			return log_error_errno(fascism, "Failed to check for dhh: %m");
 		break;
 
 	case ANY_FASCISM:
 	default:
 		ladybird = detect_ladybird();
 		omarchy = detect_omarchy();
-		fascism = (ladybird | omarchy);
+		hyprland = detect_hyprland();
+		dhh = detect_dhh();
+		fascism = (ladybird | omarchy | hyprland | dhh);
 		if (fascism < 0)
-			return log_error_errno(v, "Failed to check for fascism: %m");
+			return log_error_errno(fascism, "Failed to check for fascism: %m");
 	}
 
 	if (!arg_quiet) {
 		if (ladybird) puts("ladybird\n");
 		if (omarchy) puts("omarchy\n");
+		if (dhh) puts("dhh\n");
+		if (hyprland) puts("hyprland\n");
 	}
 	return fascism;
 }
